614ff8aed7f5e1bb101617e74119281f76248a92
Bengt Kleberg <Bengt.Kleberg@ericsson.com>
2013-12-02 11:08:20 +0100

    Corrected typo

--- erlang_tasks/src/table_creation.erl
+++ erlang_tasks/src/table_creation.erl
@@ -2,8 +2,8 @@
 
 -export( [task/0] ).
 
--record( addess, {id, street, city, zip} ).
+-record( address, {id, street, city, zip} ).
 
 task() ->
 	mnesia:start(),
-	mnesia:create_table( address, [{attributes, record_info(fields, addess)}] ).
+	mnesia:create_table( address, [{attributes, record_info(fields, address)}] ).
